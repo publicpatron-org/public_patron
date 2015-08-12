14ffb2e1cb3f65db038ad5488133e7646aba3f82
wjmaggos <wjmaggos@gmail.com>
2015-08-12 21:51:30 +0000

    make a basic User model (including secure passwords)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,11 +5,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 1
+    assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", idea_path, count: 8
     assert_select "a[href=?]", platform_path, count: 4
     assert_select "a[href=?]", about_path, count: 6
-    assert_select "a[href=?]", join_path, count: 3
+    assert_select "a[href=?]", join_path, count: 2
     assert_select "a[href=?]", support_path, count: 4
     get signup_path
   end
