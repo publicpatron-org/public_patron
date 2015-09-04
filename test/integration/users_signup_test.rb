9ce0044dfc4562bb874d2a02ce0708c96d095b65
wjmaggos <wjmaggos@gmail.com>
2015-09-04 23:05:56 +0000

    add Gravatar etc

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,6 +8,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
   end
 
   test "valid signup information" do
@@ -16,6 +18,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post_via_redirect users_path, user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.nil?
   end
 
 end
